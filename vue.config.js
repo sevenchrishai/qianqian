bd93faa678dad902406c4d53af3317f5edefc431
chris <495926158@qq.com>
2019-09-27 14:40:44 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/qianqian/'
+        ? '/qianqian/dist/'
         : '/',
     devServer: {
         port: '2278',
