5d42154ad991dfe13a4abbd55c4357b29f49d928
chris <495926158@qq.com>
2019-09-27 14:34:00 +0800

    favicon.ico

--- vue.config.js
+++ vue.config.js
@@ -14,5 +14,15 @@ module.exports = {
                 }
             }
         }
-    }
+    },
+    // 以下是pwa配置
+    pwa: {
+        iconPaths: {
+            favicon32     : 'favicon.ico',
+            favicon16     : 'favicon.ico',
+            appleTouchIcon: 'favicon.ico',
+            maskIcon      : 'favicon.ico',
+            msTileImage   : 'favicon.ico'
+        }
+    },
 }
